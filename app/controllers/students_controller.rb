73e158c90d046c88853608c16b4888ad3f1454e2
Sophie Gu <sophieqgu@gmail.com>
2020-02-05 21:43:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
     @student.active = !@student.active
     @student.save
+    redirect_to student_path(@student)
   end 
   
   private
