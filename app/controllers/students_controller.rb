c69f4103b07ae490c57411aad03d38a53ba443a1
Sophie Gu <sophieqgu@gmail.com>
2020-02-05 21:41:04 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
 
   def activate 
     @student = Student.find(params[:id])
-    @student.active = true
+    @student.active = !@student.active
   end 
   
   private
